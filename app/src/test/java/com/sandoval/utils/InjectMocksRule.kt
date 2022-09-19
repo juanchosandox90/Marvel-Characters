d5716ca4bfed3f6501c161bff8021f5c6611d38b
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2022-09-18 23:06:13 -0500

    Unit test for Data and Domain - Marvel characters list and Marvel character detail features
    - Add data unit test repository for the marvel characters list feature and marvel character detail feature
    - Add domain unit test use-case for the marvel characters list feature and marvel character detail feature
    - Add utils package for the unit test implementations. (MockK and AndroidArch)

---
+++ app/src/test/java/com/sandoval/utils/InjectMocksRule.kt
@@ -0,0 +1,12 @@
+package com.sandoval.utils
+
+import io.mockk.MockKAnnotations
+import org.junit.rules.TestRule
+
+object InjectMocksRule {
+
+    fun create(testClass: Any) = TestRule { statement, _ ->
+        MockKAnnotations.init(testClass, relaxUnitFun = true)
+        statement
+    }
+}
