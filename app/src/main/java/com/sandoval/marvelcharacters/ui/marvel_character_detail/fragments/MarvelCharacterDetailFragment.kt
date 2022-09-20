c877bbe0b53dc4556d31b6ee7950cb7a2e1d1fd5
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2022-09-20 11:47:25 -0500

    Marvel Character Detail Render UI
    - Add the marvel character detail viewmodel implementation to bring the data to be rendered in the UI for the marvel character detail
    - Add to the MarvelCharacterDetailFragment.kt a different way to render the information using the binding directly through the class.
    - As DResult is a collection but the detail of the marvel character is returning only 1 position (should be refactored to bring a class not a collection) the method to render the UI is getting the .first() position for the DResult class regarding the detail (name, description, image)
    - Add the shared icon to be used in future functionality to share the information about the marvel character to third party apps.

--- app/src/main/java/com/sandoval/marvelcharacters/ui/marvel_character_detail/fragments/MarvelCharacterDetailFragment.kt
+++ app/src/main/java/com/sandoval/marvelcharacters/ui/marvel_character_detail/fragments/MarvelCharacterDetailFragment.kt
@@ -1,9 +1,13 @@
 package com.sandoval.marvelcharacters.ui.marvel_character_detail.fragments
 
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.navArgs
+import coil.load
+import com.sandoval.marvelcharacters.R
 import com.sandoval.marvelcharacters.databinding.FragmentMarvelCharacterDetailBinding
+import com.sandoval.marvelcharacters.domain.models.marvel_character_detail.DResult
 import com.sandoval.marvelcharacters.ui.base.BaseFragment
 import com.sandoval.marvelcharacters.ui.marvel_character_detail.viewmodel.MarvelCharacterDetailViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,11 +36,45 @@ class MarvelCharacterDetailFragment : BaseFragment<FragmentMarvelCharacterDetail
                     hideLoading()
                 }
                 it.marvelCharacterDetail != null -> {
+                    when {
+                        it.marvelCharacterDetail.results.isNullOrEmpty() -> {
+                            Log.e("Marvel Character Detail", "No error found")
+                        }
+                        else -> {
+                            setupMarvelCharacterDetailView(it.marvelCharacterDetail.results)
+                        }
+                    }
+
                     hideLoading()
                 }
                 it.errorMessage != null -> {
                     hideLoading()
                 }
+            }
+        }
+
+    }
+
+    private fun setupMarvelCharacterDetailView(results: List<DResult>) {
+        binding.marvelCharacterNameDetail.text = results.first().name.toString()
+        binding.marvelCharacterDescriptionDetail.text = results.first().description.toString()
+        val thumbnailPath = results.first().thumbnail?.path
+        val thumbnailExtension = results.first().thumbnail?.extension
+
+        when {
+            thumbnailPath != null && thumbnailExtension != null -> {
+                val thumbnailUrl = "$thumbnailPath.$thumbnailExtension"
+                Log.d("ThumbnailUrl", thumbnailUrl)
+                binding.marvelCharacterImageDetail.load(thumbnailUrl) {
+                    crossfade(300)
+                    listener(onError = { _, _ ->
+                        binding.marvelCharacterImageDetail.setImageResource(R.drawable.ic_no_picture_256)
+                    })
+                }
+
+            }
+            else -> {
+                binding.marvelCharacterImageDetail.setImageResource(R.drawable.ic_no_picture_256)
             }
         }
     }
