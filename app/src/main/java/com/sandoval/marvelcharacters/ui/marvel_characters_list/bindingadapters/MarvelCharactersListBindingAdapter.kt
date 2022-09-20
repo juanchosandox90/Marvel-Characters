55a7f1e4bbad5c74053ceb2a58c084e515dba947
Juan Camilo Sandoval Devia <juansandoval222@gmail.com>
2022-09-20 00:09:33 -0500

    Render Marvel Characters List
    - Fix minor warning regarding the static method to declare a binding adapter using companion object. The fix was simple: Declare the binding adapter as a singleton and get rid of the final static strategy using the companion.

--- app/src/main/java/com/sandoval/marvelcharacters/ui/marvel_characters_list/bindingadapters/MarvelCharactersListBindingAdapter.kt
+++ app/src/main/java/com/sandoval/marvelcharacters/ui/marvel_characters_list/bindingadapters/MarvelCharactersListBindingAdapter.kt
@@ -7,34 +7,32 @@ import coil.load
 import com.sandoval.marvelcharacters.R
 import com.sandoval.marvelcharacters.ui.marvel_characters_list.models.helper.IThumbnail
 
-class MarvelCharactersListBindingAdapter {
+object MarvelCharactersListBindingAdapter {
 
-    companion object {
+    @BindingAdapter(value = ["android:loadBackgroundImgOrColor"], requireAll = true)
+    @JvmStatic
+    fun loadImageFromUrl(
+        imageView: ImageView, thumbnail: IThumbnail
+    ) {
+        val thumbnailPath = thumbnail.path
+        val thumbnailExtension = thumbnail.extension
 
-        @BindingAdapter(value = ["android:loadBackgroundImgOrColor"], requireAll = true)
-        @JvmStatic
-        fun loadImageFromUrl(
-            imageView: ImageView, thumbnail: IThumbnail
-        ) {
-            val thumbnailPath = thumbnail.path
-            val thumbnailExtension = thumbnail.extension
-
-            when {
-                thumbnailPath != null && thumbnailExtension != null -> {
-                    val thumbnailUrl = "$thumbnailPath.$thumbnailExtension"
-                    Log.d("ThumbnailUrl", thumbnailUrl)
-                    imageView.load(thumbnailUrl) {
-                        crossfade(300)
-                        listener(onError = { _, _ ->
-                            imageView.setImageResource(R.drawable.ic_no_picture_256)
-                        })
-                    }
-
+        when {
+            thumbnailPath != null && thumbnailExtension != null -> {
+                val thumbnailUrl = "$thumbnailPath.$thumbnailExtension"
+                Log.d("ThumbnailUrl", thumbnailUrl)
+                imageView.load(thumbnailUrl) {
+                    crossfade(300)
+                    listener(onError = { _, _ ->
+                        imageView.setImageResource(R.drawable.ic_no_picture_256)
+                    })
                 }
-                else -> {
-                    imageView.setImageResource(R.drawable.ic_no_picture_256)
-                }
+
+            }
+            else -> {
+                imageView.setImageResource(R.drawable.ic_no_picture_256)
             }
         }
     }
+
 }
